bde20c14a8a67d2fb46272b43b7a60f132eeebc9
TANAV <tanavsaraf@gmail.com>
2021-04-23 19:57:57 +0530

    C47

--- js/game.js
+++ js/game.js
@@ -29,10 +29,10 @@ class Game
                 player.getPlayerCount();
             }
             form=new Form()
-            player1=createSprite(100,350,20,70);
+            player1=createSprite(100,350,20,140);
            
             
-            player2=createSprite(700,350,20,70);
+            player2=createSprite(700,350,20,140);
             
             
             form.display()
@@ -51,12 +51,13 @@ class Game
             push()
             translate(100,200);
             rotate(allPlayers["player1"].angle)
-            image(player1Img,0,0,20,20);
+            image(player1Img,0,0,70,70);
             pop()
 
             push()
             translate(700,200);
             rotate(allPlayers["player2"].angle)
+            image(player2Img,0,0,70,70);
             rect(0,0,20,20);
             pop()
             
@@ -84,18 +85,42 @@ class Game
                 }else if(player.index===2)
                 {
                   arrow = createSprite(700,200,70,9);
+                  
             
                 }
-                arrow.pointTo(400,200+player.angle*2)      
+                arrow.pointTo(400,player.angle*2)      
                 arrowGroup.add(arrow);
                 player.arrow-=1
-                arrow.velocityX=player.vX
-                arrow.velocityY=player.vY
+                arrow.velocityX=7;
+                arrow.velocityY=0;
                 player.update()
                 
+
                }
-            
-            
+                    
+               if(player.index===1)
+               {
+                if(arrowGroup.isTouching(player2))
+                {
+                    allPlayers["player2"].health-=1
+                    db.ref("players/player2").update({
+                        health:allPlayers["player2"].health
+                    })
+                }
+               }else if(player.index===2)
+               {
+                if(arrowGroup.isTouching(player1))
+                {
+                    allPlayers["player1"].health-=1
+                    db.ref("players/player1").update({
+                        health:allPlayers["player1"].health
+                    })
+                }
+               }
+               if(player.health===0)
+               {
+                   gameState=2
+               }
             
         }
        
